1359e757c8fda953b23baf790ae385c01e207c7d
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-06-30 11:21:54 +0300

    navbar ready

--- src/utils/Navbar.tsx
+++ src/utils/Navbar.tsx
@@ -1,18 +1,85 @@
 import React, {FC} from "react";
 import styles from "./navstyles.scss"
-import {menuItems} from "../components/Header/menu";
 
 const Navbar: FC = () => {
   return (
     <nav>
       <ul className={styles.menus}>
-        {menuItems.map((menu, index) => {
-          return (
-            <li className={styles.menuItems} key={index}>
-              <a href={menu.link}>{menu.title}</a>
-            </li>
-          );
-        })}
+        <div className={styles.wrapper}>
+          <li className={styles.menuItems}>
+            <a href="https://www.unrealengine.com/unreal-engine-5">Products</a>
+          </li>
+          <div className={styles.dropdown}>
+            <ul>
+              <li>
+                <a href="https://www.unrealengine.com/unreal-engine-5">Unreal Engine 5</a>
+              </li>
+              <li>
+                <a href="https://www.unrealengine.com/features">Features</a>
+              </li>
+              <li>
+                <a href="https://www.unrealengine.com/license">Licensing options</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.wrapper}>
+          <li className={styles.menuItems}>
+            <a href="https://www.unrealengine.com/games">Solutions</a>
+          </li>
+          <div className={styles.dropdown}>
+            <ul>
+              <li>
+                <a href="https://www.unrealengine.com/solutions/games">Games</a>
+              </li>
+              <li>
+                <a href="https://www.unrealengine.com/solutions/film-television">Film & television</a>
+              </li>
+              <li>
+                <a href="https://www.unrealengine.com/solutions/animation">Animation</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.wrapper}>
+          <li className={styles.menuItems}>
+            <a href="https://www.unrealengine.com/learn">Learn</a>
+          </li>
+          <div className={styles.dropdown}>
+            <ul>
+              <li>
+                <a href="https://www.unrealengine.com/learn">Learning resources</a>
+              </li>
+              <li>
+                <a href="https://www.unrealengine.com/educators">Unreal Engine for educators</a>
+              </li>
+              <li>
+                <a href="https://www.unrealengine.com/students">Unreal Engine for students</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.wrapper}>
+          <li className={styles.menuItems}>
+            <a href="https://www.unrealengine.com/creators">More</a>
+          </li>
+          <div className={styles.dropdown}>
+            <ul>
+              <li>
+                <a href="https://www.unrealengine.com/feed">News & Events</a>
+              </li>
+              <li>
+                <a href="https://www.unrealengine.com/creators">Community</a>
+              </li>
+              <li>
+                <a href="https://www.unrealengine.com/support">Support</a>
+              </li>
+              <li>
+                <a href="https://www.unrealengine.com/marketplace/store">Marketplace</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </ul>
     </nav>
   );
